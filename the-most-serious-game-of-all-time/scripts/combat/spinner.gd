73afd0edda9d6043a859a50f9423a54f1a6097bf
Hannah <covtau@disroot.org>
2026-06-20 18:42:43 -0800

    Make SpinnerCollection adaptive; optimization

--- the-most-serious-game-of-all-time/scripts/combat/spinner.gd
+++ the-most-serious-game-of-all-time/scripts/combat/spinner.gd
@@ -1,40 +1,37 @@
 class_name Spinner
 extends Node2D
 
+## Should the spinner point left instead of right?
+@export var left: bool = false
 ## Radius of the spinner wheel
 @export var size: float = 50
 ## Cards contained by the wheel
 @export var data: SpinnerData
 var wheel: Node2D
 
+
 func _ready() -> void {
 	refresh_wheel()
 }
 
-func refresh_wheel() -> void {
-	if wheel == null {
-		wheel = Node2D.new()
-		add_child(wheel)
-	}
-	
-	for child in wheel.get_children() {
-		child.queue_free()
-	}
-	
+## Only adjust the polygon points
+## for UI adaptive resizing
+func resize_wheel() -> void {
 	var card_amt := data.cards.size()
 	var slice_size := TAU/card_amt
-	for idx in range(card_amt) {
-		var card = data.cards[idx]
-		var slice := Polygon2D.new()
+	var idx := 0
+	for tentative_slice in wheel.get_children() {
+		if tentative_slice is not Polygon2D {
+			continue
+		}
+		var slice := tentative_slice as Polygon2D
 		
 		# Need to store polygon arrays in a seperate variable and then set them
 		# otherwise they won't be saved
 		var temp_points: Array[Vector2]
-		var temp_colors: Array[Color]
 		
 		# Slice tip
 		temp_points.append(Vector2.ZERO)
-		temp_colors.append(Color.BLACK)
 		# Edge
 		@warning_ignore("integer_division") # Intended
 		var ang_count = maxi(2, 100/card_amt)
@@ -47,13 +44,31 @@ func refresh_wheel() -> void {
 				cos(ang) * size,
 				sin(ang) * size
 			))
-			temp_colors.append(card.color)
 		}
 		
 		# Now we can finally re-add the array
 		slice.polygon = temp_points
-		slice.vertex_colors = temp_colors
 		
+		idx += 1
+	}
+}
+
+## Recreate the entire spinner
+func refresh_wheel() -> void {
+	if wheel == null {
+		wheel = Node2D.new()
+		add_child(wheel)
+	}
+	
+	for child in wheel.get_children() {
+		child.queue_free()
+	}
+	
+	for card in data.cards {
+		var slice := Polygon2D.new()
+		slice.color = card.color
 		wheel.add_child(slice)
 	}
+	
+	resize_wheel()
 }
--- the-most-serious-game-of-all-time/scripts/combat/spinner_collection.gd
+++ the-most-serious-game-of-all-time/scripts/combat/spinner_collection.gd
@@ -1,16 +1,34 @@
-extends Node2D
+extends Control
 
-@export var height: float = 630
+@export var left: bool = false
 @export var padding: float = 10.0
 @export var max_wheel_size: float = 100
 @export var spinners: Array[SpinnerData]:
 	set(data):
 		spinners = data
 		refresh_spinners()
+@export var overlays: Array[Texture2D]
 @export var spinner_nodes: Array[Spinner]
 
 func _ready() -> void {
+	
 	refresh_spinners()
+}
+
+func _process(delta: float) -> void {
+	var spinner_count := spinners.size()
+	var padding_count := maxi(0, spinner_count-1)
+	var wheel_height := minf(max_wheel_size*2, (size.y - padding*padding_count)/(spinner_count as float))
+	var total_height := wheel_height*spinner_count + padding*padding_count
+	var pos := (size.y-total_height)/2 + wheel_height/2
+	var pos_inc := wheel_height+padding
+	for spinner in spinner_nodes {
+		spinner.left = left
+		spinner.size = wheel_height/2
+		spinner.position.y = pos
+		spinner.resize_wheel()
+		pos += pos_inc
+	}
 }
 
 func refresh_spinners() -> void {
@@ -28,17 +46,5 @@ func refresh_spinners() -> void {
 			spinner_nodes[i].queue_free()
 			spinner_nodes.pop_back()
 		}
-	}
-	
-	var padding_count := maxi(0, spinner_count-1)
-	var wheel_height := minf(max_wheel_size*2, (height - padding*padding_count)/(spinner_count as float))
-	var real_height := wheel_height*spinner_count + padding*padding_count
-	var pos := -real_height/2 + wheel_height/2
-	var pos_inc := wheel_height+padding
-	for spinner in spinner_nodes {
-		spinner.refresh_wheel()
-		spinner.size = wheel_height/2
-		spinner.position.y = pos
-		pos += pos_inc
 	}
 }
